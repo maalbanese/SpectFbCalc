0ec36d0d79d97d08814c05e6a31d763790ba8123
deborahrotoli <d.rotoli@isac.cnr.it>
2026-06-10 14:29:44 +0000

    Remove other files from repo

--- spectfbcalc/prova.py
+++
@@ -1,7 +0,0 @@
-import spectfbcalc_lib as sfc
-
-config_file = '/perm/ecme5989/SpectFbCalc/spectfbcalc/config_r8.yaml'
-control, experiment, kernel = sfc.preprocess_data(config_file)
-cart_out='/perm/ecme5989/new_func_prove/model_1/remapped_HUANG/'
-
-rad=sfc.Rad_anomaly_planck_surf(experiment, kernel, cart_out )
